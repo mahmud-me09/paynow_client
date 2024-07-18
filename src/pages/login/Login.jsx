3659a14e6f00f4540fd1df3b3960029972c14959
mahmud-me09 <mahmud.me09@gmail.com>
2024-07-18 08:29:55 +0600

    login section completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useAxiosSecure from '../../utils/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const Login = () => {
-    function handleSubmit (event){
-        event.preventDefault()
-
-    }
+    const axiosSecure = useAxiosSecure();
+	const navigate = useNavigate()
+	let errorMessage = "";
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const data = {
+			id: form.id.value,
+			pin: form.password.value,
+		};
+		axiosSecure
+			.post("/login",data)
+			.then((res) => {
+				console.log(res.data);
+				if (res.data.status === 401) {
+					Swal.fire({
+						icon: "error",
+						title: "Login Failed",
+						text: `${res.data.message}`,
+					});
+				}else{
+					navigate('/user')
+				}
+				form.reset();
+			})
+			.catch((error) => console.log(error.message));
+	};
     return (
 		<div className="border p-20 bg-green-300 rounded-lg">
-			<h1 className="text-3xl ">
-				Login to <br /> your PayNow Account
+			<h1 className="text-3xl text-center">
+				Login to <br /> your <span className='font-bold'>PayNow</span> Account
 			</h1>
 			<form className="" onSubmit={handleSubmit}>
 				<div className="flex flex-col my-3">
@@ -40,7 +65,7 @@ const Login = () => {
 			</form>
 			<div className="pt-4 text-center">
 				If you don't register yet{" "}
-				<p className='text-blue-700 underline'>
+				<p className="text-blue-700 underline">
 					<Link to="/registration">Register Here</Link>
 				</p>
 			</div>
